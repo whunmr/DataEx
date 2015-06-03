4d8dea551ba16a11aae73ec2b74b918d5c243845
whunmr <whunmr@gmail.com>
2015-06-03 13:07:15 +0800

    add dataex:array.

--- test/dataex_utility.h
+++ test/dataex_utility.h
@@ -6,6 +6,7 @@ template <>     struct enable_if<true> {
   typedef void type;
 };
 
+/*----------------------------------------------------------------------------*/
 template <class B, class D> struct IsBaseOf {
   typedef char (&Small)[1];
   typedef char (&Big)[2];
@@ -19,5 +20,29 @@ template <class B, class D> struct IsBaseOf {
 
 #define __is_base_of(Base, Derived) IsBaseOf<const Base*, const Derived*>::value
 
+/*----------------------------------------------------------------------------*/
+namespace dataex {
+template<typename T, size_t N>
+  struct array{
+    T elems[N];
+    
+    typedef T&             reference;
+    typedef const T&       const_reference;
+    
+    reference operator[](size_t i) { 
+        assert(("out of range", i < N));
+        return elems[i];
+    }
+    
+    const_reference operator[](size_t i) const {     
+        assert(("out of range", i < N));
+        return elems[i]; 
+    }
+  
+    const T* data() const { return elems; }
+    T* data() { return elems; }
+    T* c_array() { return elems; }
+  };
+} //namespace dataex
 
 #endif //#ifndef __DATAEX_UTILITY_H__
