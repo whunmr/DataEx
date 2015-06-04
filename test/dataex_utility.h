e744a6fe5dabf30185a5c8987a5d349c142c8ea8
whunmr <whunmr@gmail.com>
2015-06-04 12:49:56 +0800

    x

--- test/dataex_utility.h
+++ test/dataex_utility.h
@@ -1,8 +1,11 @@
 #ifndef __DATAEX_UTILITY_H__
 #define __DATAEX_UTILITY_H__
 
-template <bool> struct enable_if {};
-template <>     struct enable_if<true> {
+template <bool>
+struct enable_if {};
+
+template <>
+struct enable_if<true> {
   typedef void type;
 };
 
@@ -44,5 +47,34 @@ template<typename T, size_t N>
     T* c_array() { return elems; }
   };
 } //namespace dataex
+
+/*----------------------------------------------------------------------------*/
+template<typename T, size_t size>
+::testing::AssertionResult ArraysMatch(const T (&expected)[size],
+                                       const char* actual){
+    for (size_t i(0); i < size; ++i){
+        if (expected[i] != actual[i]){
+            return ::testing::AssertionFailure() << "array[" << i
+                << "] (" << actual[i]  << "[as int]: "<< (int)actual[i]
+                << ") != expected[" << i
+                << "] (" << expected[i] << "[as int]: " << (int)expected[i] << ")";
+        }
+    }
+    return ::testing::AssertionSuccess();
+}
+
+/*----------------------------------------------------------------------------*/
+#ifdef _MSC_VER
+  #if defined(_M_IX86) && !defined(DATAEX_DISABLE_LITTLE_ENDIAN_OPT_FOR_TEST)
+    #define DATAEX_LITTLE_ENDIAN 1
+  #endif
+#else
+  #include <sys/param.h>   // __BYTE_ORDER
+  #if ((defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)) || \
+         (defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN)) && \
+      !defined(DATAEX_DISABLE_LITTLE_ENDIAN_OPT_FOR_TEST)
+    #define DATAEX_LITTLE_ENDIAN 1
+  #endif
+#endif
 
 #endif //#ifndef __DATAEX_UTILITY_H__
